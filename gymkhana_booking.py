20559e9c4e9cd7959873866516cc7c5882dd1707
Paras Lohani <66436087+paras2411@users.noreply.github.com>
2021-02-24 12:10:20 +0530

    add headless code

--- gymkhana_booking.py
+++ gymkhana_booking.py
@@ -1,5 +1,6 @@
 
 from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
 from time import sleep
 import yaml
 import datetime
@@ -20,12 +21,22 @@ with open("booking.yaml", 'r') as stream:
         exit(0)
 
 if 'firefox' in booking_details['browser']:
-    driver = webdriver.Firefox(executable_path=booking_details['browser']['firefox'])
-elif 'chrome' in booking_details['chrome']:
+
+    if booking_details['headless']:
+        opts = FirefoxOptions()
+        opts.add_argument("--headless")
+        driver = webdriver.Firefox(options=opts)
+    else:
+        driver = webdriver.Firefox(executable_path=booking_details['browser']['firefox'])
+
+elif 'chrome' in booking_details['browser']:
+
     driver = webdriver.Chrome(executable_path=booking_details['browser']['chrome'])
+
 else:
+
     driver = {}
-    print('browser option does not exist')
+    print('{} browser option does not exist'.format(booking_details['browser']))
     exit(0)
 
 driver.get("https://iiscgym.iisc.ac.in/gfr/")
@@ -33,6 +44,8 @@ driver.get("https://iiscgym.iisc.ac.in/gfr/")
 driver.find_element_by_xpath("//*[@id='email']").send_keys(booking_details['login']['username'])
 driver.find_element_by_xpath("//*[@id='pass']").send_keys(booking_details['login']['password'])
 driver.find_element_by_xpath("//*[@id='login']").click()
+
+acknowledged = False
 
 for facility in booking_details['facility']:
 
@@ -44,7 +57,7 @@ for facility in booking_details['facility']:
         label = 3
     else:
         label = 0
-        print('facility {} does not exist', facility)
+        print('Facility {} does not exist', facility)
         exit(0)
 
     driver.find_element_by_xpath('/html/body/div[2]/div[4]/div/div/div[2]/div/label[{}]'.format(label)).click()
@@ -75,6 +88,7 @@ for facility in booking_details['facility']:
     driver.find_element_by_xpath('//*[@id="btn_chk"]').click()
     counter = 1
 
+    skip_booking = False
     # Slot is full
     while driver.find_element_by_xpath('//*[@id="msg_box"]').text == 'Time slot not selected !':
 
@@ -85,7 +99,8 @@ for facility in booking_details['facility']:
         if (time + counter > booking_details['number_of_slots'][facility] and time - counter < 1) or \
                 counter > booking_details['lookup_range']:
             print('No slot available for {} in your booking range'.format(facility))
-            exit(0)
+            skip_booking = True
+            break
 
         if 'before' in booking_details['lookup'] and time - counter > 0:
             driver.find_element_by_xpath('/html/body/div[2]/div[4]/div/div/div[3]/div[{}]/div[1]'.
@@ -106,9 +121,14 @@ for facility in booking_details['facility']:
 
         counter += 1
 
+    if skip_booking:
+        continue
+
     sleep(1)
-    for i in range(1, 5):
-        driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div/div[2]/ul/li[{}]/div/label'.format(i)).click()
+    if not acknowledged:
+        for i in range(1, 5):
+            driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div/div[2]/ul/li[{}]/div/label'.format(i)).click()
+        acknowledged = True
 
     driver.find_element_by_xpath('//*[@id="btn_add"]').click()
 
